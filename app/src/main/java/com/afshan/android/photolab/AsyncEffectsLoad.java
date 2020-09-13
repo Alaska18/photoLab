40328119d7945aa642b10dc1ac19956cca439ace
Alaska <afshanahmed.ec18@rvce.edu.in>
2020-09-13 13:28:14 +0530

    PICS added

---
+++ app/src/main/java/com/afshan/android/photolab/AsyncEffectsLoad.java
@@ -0,0 +1,14 @@
+package com.afshan.android.photolab;
+
+import android.graphics.Bitmap;
+import android.os.AsyncTask;
+
+public class AsyncEffectsLoad extends AsyncTask<Void, Void, Bitmap>
+{
+
+    @Override
+    protected Bitmap doInBackground(Void... voids)
+    {
+        return null;
+    }
+}
